e49dbd439f3462b29b21a93be86bacf55647201c
Ace.Chandler <phatmanace@gmail.com>
2015-05-10 22:19:53 +0100

    Updating Grammar

--- src/com/acemanstatic/TestRig.java
+++ src/com/acemanstatic/TestRig.java
@@ -2,7 +2,9 @@ package com.acemanstatic;
 
 import com.aceman.SenatorLexer;
 import com.aceman.SenatorParser;
+import com.acemanstatic.visitors.VarListener;
 import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 import java.io.FileInputStream;
 
@@ -13,13 +15,31 @@ public class TestRig {
         SenatorLexer lexer = new SenatorLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
         SenatorParser parser = new SenatorParser(tokens);
-        parser.setBuildParseTree(true);
-        SenatorParser.StmtContext tree  = parser.stmt();
+       // parser.setBuildParseTree(true);
 
+        VarListener vl =new VarListener();
+        ParseTreeWalker walker = new ParseTreeWalker();
+        walker.walk(vl, parser.stmt());
+
+        if(vl.areErrors()){
+            System.out.println("Compilation Errors");
+            for(String s : vl.getErrors()){
+                System.out.println(String.format("Error Coco>> %s", s));
+            }
+            System.out.println("Not proceeding further");
+            System.exit(-1);
+        }
+
+        parser.reset();
+        SenatorParser.StmtContext tree  = parser.stmt();
         InitialParseVisitor visitor = new InitialParseVisitor();
+
+
+
         Object returned = visitor.visit(tree);
         visitor.getProgram().exec(0);
 
         System.out.println("done: " + returned);
+
     }
 }
