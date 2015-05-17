5371d9f0f5a02010bd8a50ecd5f77c9dcc9a95e5
Ace.Chandler <phatmanace@gmail.com>
2015-05-17 22:06:16 +0100

    Adding VL

--- src/com/acemanstatic/visitors/VarLIstener.java
+++ src/com/acemanstatic/visitors/VarLIstener.java
@@ -3,17 +3,18 @@ package com.acemanstatic.visitors;
 import com.aceman.SenatorBaseListener;
 import com.aceman.SenatorParser;
 import com.acemanstatic.Variables.Var;
-import org.antlr.v4.runtime.misc.NotNull;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * Created by phatmanace on 09/05/2015.
  */
 public class VarListener extends SenatorBaseListener{
 
+    Logger logger = Logger.getLogger(VarListener.class.getName());
     private Hashtable<String, Var> vars = new Hashtable<>();
     private List<String> errors = new ArrayList<>();
 
@@ -25,14 +26,14 @@ public class VarListener extends SenatorBaseListener{
     @Override
     public void enterAssignvar( SenatorParser.AssignvarContext ctx) {
         if(ctx.getParent() == null){
-            System.out.println("Printing this from a root node");
+            logger.info("Printing this from a root node");
         }else{
-            System.out.print(String.format("Printing this from a %s",  ctx.getParent().getRuleContext().toStringTree()));
+           // logger.info(String.format("Printing this from a %s",  ctx.getParent().getRuleContext().toStringTree()));
 
         }
         String varName  = ctx.getChild(1).getText();
         String varValue = ctx.getChild(3).getText();
-        System.out.println(String.format("Setting %s to %s", varName, varValue));
+        logger.info(String.format("Setting %s to %s", varName, varValue));
         if(vars.containsKey(varName)){
             errors.add(String.format("Redefinition of var %s at %d ", varName, ctx.getStart().getStartIndex()));
         }else {
@@ -48,6 +49,10 @@ public class VarListener extends SenatorBaseListener{
 
     public List<String> getErrors(){
         return errors;
+    }
+
+    public int getCount(){
+        return vars.size();
     }
 
 
